393526bc7dd2ff128f86c565fe7301af6ae5f58c
Ari Tomas Bussi <abussy@gmail.com>
2022-09-01 21:06:36 +0200

    store and views setup

---
+++ to-do-ironhack/src/views/AuthView.vue
@@ -0,0 +1,34 @@
+<script>
+import { mapState, mapActions } from 'pinia';
+import userStore from '@/store/auth';
+
+export default {
+  name: 'AuthView',
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
+  methods: {
+    ...mapActions(userStore, ['signUp']),
+    handleSignUp() {
+      const userData = {
+        email: 'lala@lala.com',
+        password: '123456',
+      };
+
+      this.signUp(userData.email, userData.password);
+    },
+  },
+  watch: {
+    user() {
+      if (this.user) {
+        this.$router.push({ path: '/' });
+      }
+    },
+  },
+};
+</script>
+
+<template>
+    <h1>Auth view</h1>
+    <button @click="handleSignUp">Register</button>
+</template>

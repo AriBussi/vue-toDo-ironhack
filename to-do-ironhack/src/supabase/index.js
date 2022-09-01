6223e473e447a9fe20230606777cdec1e2d1440d
Ari Tomas Bussi <abussy@gmail.com>
2022-09-01 19:59:23 +0200

    add pinia config

--- to-do-ironhack/src/supabase/index.js
+++ to-do-ironhack/src/supabase/index.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://boqmlgjcihzvwntanocz.supabase.co';
 const supabaseAnonToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvcW1sZ2pjaWh6dndudGFub2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIwNTA4MjEsImV4cCI6MTk3NzYyNjgyMX0.frWttOseMqQr2z1KGHmw_Ba9SeHD4JODUjovMm5V5PQ';

f9fcba4bbec12b2cf33c1bc1e3e103d0a9366d66
Ari Tomas Bussi <abussy@gmail.com>
2022-09-03 13:24:47 +0200

    refactor: use composition api on tasks store

--- to-do-ironhack/src/store/tasks.js
+++ to-do-ironhack/src/store/tasks.js
@@ -1,17 +1,30 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import supabase from '../supabase/index';
 
-export default defineStore('tasks', {
-  state: () => ({
-    tasks: [],
-  }),
-  actions: {
-    async fetchTasks() {
-      const { data: tasks } = await supabase
-        .from('tasks')
-        .select('*')
-        .order('id', { ascending: false });
-      this.tasks = tasks;
+export default defineStore('tasks', () => {
+  const currentTasks = ref([
+    {
+      id: 1,
+      title: 'Create a toDo app',
+      description: 'https://my.ironhack.com/cohorts/626174a718a13d002ccb605b/lms/courses/course-v1:IRONHACK+FEWDPT+2022_06_20_CONFORCAT/modules/ironhack-course-chapter_11/units/ironhack-course-chapter_11-sequential-vertical',
     },
-  },
+    {
+      id: 2,
+      title: 'Find a place to live',
+    },
+  ]);
+
+  const fetchTasks = async () => {
+    const { data: tasks } = await supabase
+      .from('tasks')
+      .select('*')
+      .order('id', { ascending: false });
+    tasks.value = tasks;
+  };
+
+  return {
+    currentTasks,
+    fetchTasks,
+  };
 });

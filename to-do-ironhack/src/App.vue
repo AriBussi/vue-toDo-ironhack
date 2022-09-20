2b502e45e6faf0a9761a3b9736f90475f6996469
Ari Tomas Bussi <abussy@gmail.com>
2022-09-20 20:36:20 +0200

    feat: add footer component

--- to-do-ironhack/src/App.vue
+++ to-do-ironhack/src/App.vue
@@ -3,6 +3,7 @@ import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import userStore from '@/store/auth';
 import MainNavigation from '@/components/MainNavigation.vue';
+import Footer from '@/components/TheFooter.vue';
 
 const store = userStore();
 const router = useRouter();
@@ -24,7 +25,10 @@ onMounted(async () => {
 
 <template>
   <MainNavigation />
-  <router-view/>
+  <main>
+    <router-view/>
+  </main>
+  <Footer />
 </template>
 
 <style>
@@ -34,7 +38,10 @@ onMounted(async () => {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: var(--neutral--dark);
-  max-height: 100vh;
   margin-top: 3.5rem;
+}
+
+main {
+  min-height: calc(100vh - 5rem);
 }
 </style>

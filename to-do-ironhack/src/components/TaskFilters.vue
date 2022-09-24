96ec0f74cb2fa89e776280c3a49c96834df6e218
Ari Tomas Bussi <abussy@gmail.com>
2022-09-24 12:01:04 +0200

    feat: add filter on main nav

---
+++ to-do-ironhack/src/components/TaskFilters.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<script setup>
+
+</script>
+
+<template>
+    <span >
+        <font-awesome-icon icon="fa-solid fa-filter" size="lg" />
+        <label for="filters">
+            <select  name="filters" id="filters">
+                <option value="all">All tasks</option>
+                <option value="completed">Complete</option>
+                <option value="active">Ongoing</option>
+            </select>
+        </label>
+    </span>
+</template>
+
+<style scoped>
+    select {
+        appearance: none;
+        border: none;
+        color: inherit;
+        font-family: inherit;
+        font-weight: inherit;
+        font-size: inherit;
+        outline: none;
+        text-align: center;
+    }
+</style>
